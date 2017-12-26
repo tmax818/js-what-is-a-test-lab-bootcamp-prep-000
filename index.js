da2b24dabc27c60fb98dcaaf605649324b0c336d
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-26 23:24:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = ` is ${height} inches tall`
+var message = ` is ${height} inches tall and name is ${name}`
 
 module.exports = {name, height, message}
